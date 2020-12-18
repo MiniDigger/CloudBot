de7be3e2142f931b5ebb7a6728f80ded32941853
simpleauthority <jacob@algorithmjunkie.com>
2020-12-18 02:45:24 -0800

    use built in zci method instead

--- plugins/duckduckgo.py
+++ plugins/duckduckgo.py
@@ -18,25 +18,4 @@ from cloudbot.util import formatting
 @hook.command('ddg', 'duckduckgo', 'g', 'google')
 def search(text):
     """<query> -- Returns first DDG instant answer result for <query>."""
-
-    no_results = "No results found."
-
-    query = duckduckgo.query(text)
-
-    if query.type == 'nothing':
-        return no_results
-
-    try:
-        result = query.related[0]
-    except KeyError:
-        return no_results
-
-    text = result.text
-    url = result.url
-
-    if not text:
-        text = 'No description available.'
-    else:
-        text = formatting.truncate_str(text.replace('\n', ''), 150)
-
-    return u'{} -- \x02{}'.format(url, text)
+    return u'{}'.format(duckduckgo.get_zci(text))
