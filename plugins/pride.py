3c3f41ce8470ac72a1bfe002249000db24726b48
Jacob Andersen <jacob@algorithmjunkie.com>
2021-06-01 10:30:31 -0700

    Update pride.py

--- plugins/pride.py
+++ plugins/pride.py
@@ -5,5 +5,5 @@ from cloudbot import hook
 @asyncio.coroutine
 @hook.command
 def pride(action):
-    action("June is LGBT Pride Month 2021! That is why there are rainbows. Join the celebration! See more: https://nationaltoday.com/pride-month/")
+    action("June is LGBTQ Pride Month! That is why there are rainbows right now. Join the celebration! See more: https://nationaltoday.com/pride-month/")
     
