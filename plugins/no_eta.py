957e78c0f3168e7e2e9adff2f9c68169c4fb3a33
Jacob Andersen <jacob@algorithmjunkie.com>
2021-12-02 11:00:21 -0800

    Update no_eta.py

--- plugins/no_eta.py
+++ plugins/no_eta.py
@@ -3,7 +3,7 @@ import asyncio
 from cloudbot import hook
 
 @asyncio.coroutine
-@hook.command
+@hook.command("no_eta", "eta")
 def no_eta(action):
     action("There is no ETA (Estimated Time of Arrival)! Updates to Paper do not have any sort of estimate for when they release, ever. Any and all updates will arrive when they are ready, and the only thing to do is wait for them patiently along with everyone else.")
     
